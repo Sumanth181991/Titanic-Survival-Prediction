a6a781c869c6a3b8da9d051536f9c4395e533a00
Sumanth Ashwath <sumantcool54@gmail.com>
2026-06-30 16:10:08 +0530

    FINAL API

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/predictions"
+DATABASE_URL = "postgresql://admin:admin123@postgres:5432/breast_cancer_db"
 
 engine = create_engine(DATABASE_URL)
 
